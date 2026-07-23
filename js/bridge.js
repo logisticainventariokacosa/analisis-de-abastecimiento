4bbe8d59c337731216a3e4400e2dbdc0d1f7e6fc
logisticainventariokacosa <logisticainventariokacosa@gmail.com>
2026-07-23 00:01:58 -0400

    Change BRIDGE_URL to new Apps Script endpoint
    Updated the BRIDGE_URL to a new implementation endpoint.

--- js/bridge.js
+++ js/bridge.js
@@ -2,7 +2,7 @@
 // Punto único de comunicación con el Apps Script (Gemini, Sheets, Drive, Email).
 
 // URL de tu implementación /exec del Apps Script
-const BRIDGE_URL = "https://script.google.com/macros/s/AKfycbzjZYSRY8xckogtOSc_mYr2GiwGvVToUmoAI0r_MRa6GHaGdcrItdKnJrM7ivtzYV4ePg/exec";
+const BRIDGE_URL = "https://script.google.com/macros/s/AKfycbz7G8aPhnqKCegiHvWsexY3hkw4X3sE0pah6Tyog-jTrMHHaKGderHJ4XSNR2phs9usCw/exec";
 
 // Debe coincidir EXACTAMENTE con la propiedad APP_TOKEN que pusiste en el Apps Script
 const APP_TOKEN = "kacosa2026dr";
