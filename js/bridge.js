081be3ac4a2b39c5faa059770af2947f8c385aa3
logisticainventariokacosa <logisticainventariokacosa@gmail.com>
2026-08-09 14:08:14 -0400

    Update bridge.js

--- js/bridge.js
+++ js/bridge.js
@@ -2,7 +2,7 @@
 // Punto único de comunicación con el Apps Script (Gemini, Sheets, Drive, Email).
 
 // URL de tu implementación /exec del Apps Script
-const BRIDGE_URL = "https://script.google.com/macros/s/AKfycbwXidH7LvsTEdueFQLwCWE52GpbsAqub-gghseuPj4xoExBS29-2ieCfZM2r3mz9PbUyg/exec";
+const BRIDGE_URL = "https://script.google.com/macros/s/AKfycbwnfGwksn44hntYUI6Uwjb61SNJDkQ4Oy7_y4RfEAVyq-gistQH-c_8k1V-a5vfmr4Y/exec";
 
 // Debe coincidir EXACTAMENTE con la propiedad APP_TOKEN que pusiste en el Apps Script
 const APP_TOKEN = "kacosa2026dr";
